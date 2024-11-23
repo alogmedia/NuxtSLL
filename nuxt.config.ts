ad051e91a3e691b3e28b031974d0245c6c58f142
allo-mir <allo@mir-robots.com>
2024-11-23 16:36:59 +0100

    fix: unexpected char

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
       allowedRoutes: [
         "/",
         "/artillery",
-        "/privacypolicy",´
+        "/privacypolicy",
         "/blog/.*",
         "/api/.*",
       ]
