f17c34aa7c44017db95237f47b640fc649f033e0
allo-mir <allo@mir-robots.com>
2024-11-24 16:23:40 +0100

    fix: firebase fix

--- utils/firebaseAdmin.ts
+++ utils/firebaseAdmin.ts
@@ -1,19 +1,18 @@
-import admin from "firebase-admin";
+import { initializeApp, cert } from "firebase-admin/app";
+import { getDatabase } from "firebase-admin/database";
 
 const serviceAccount = {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\n/g, "\n"),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL:
-      "https://sllvoting-default-rtdb.europe-west1.firebasedatabase.app/",
-  });
-}
+initializeApp({
+  credential: cert(serviceAccount),
+  databaseURL:
+    "https://sllvoting-default-rtdb.europe-west1.firebasedatabase.app/",
+});
 
-const db = admin.database();
-export { admin, db };
+const db = getDatabase();
+export { db };
